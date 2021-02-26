2bf087817bcee67a8ad0ec30ece48ee0839dda66
Neel Chauhan <68890430+chauhanneel@users.noreply.github.com>
2021-02-26 17:42:44 +0530

    Neel simple calculator
    New calculator is made

---
+++ neel-Simple-Interest-Calculator-master/script.js
@@ -0,0 +1,9 @@
+function compute()
+{
+    p = document.getElementById("principal").value;
+    
+}
+var principal= principal;
+var rate = rate;
+var year = year;
+var intrest= principal * years * rate / 100;
